be6ee3b64fbcf6f4d08d400cc111184bad9dca53
Anjana Tissera <anjanat@gmail.com>
2015-06-29 23:29:29 +0700

    Initial commit

--- OpenCCTVServer/src/analytic/xml/AnalyticMessage.hpp
+++ OpenCCTVServer/src/analytic/xml/AnalyticMessage.hpp
@@ -16,26 +16,33 @@ namespace xml {
 const std::string OPERATION_START_ANALYTIC = "startanalytic";
 const std::string OPERATION_STOP_ANALYTIC = "stopanalytic";
 const std::string OPERATION_KILL_ALL_ANALYTICS = "killallanalytics";
+const std::string OPERATION_UNKNOWN = "unknown";
 
 class AnalyticMessage {
 public:
+	//Request Extract
 	static std::string extractAnalyticRequestOperation(const std::string& sAnalyticRequest);
+	static void extractAnalyticStartRequestData(const std::string& sAnalyticStartRequest, unsigned int& iAnalyticInstanceId, std::string& sAnalyticDirLocation, std::string& sAnalyticFilename);
+	static void extractAnalyticStopRequestData(const std::string& sAnalyticStartRequest, unsigned int& iAnalyticInstanceId);
 
-	static std::string getAnalyticStartRequest(unsigned int iAnalyticInstanceId, const std::string& sAnalyticPluginDirLocation, const std::string& sAnalyticPluginFilename);
-	static std::string getAnalyticStartReply(const std::string& sAnalyticQueueInAddress,const std::string& sAnalyticQueueOutAddress);
+	//Reply Extract
+	static void extractAnalyticStartReplyData(const std::string& sAnalyticStartReply, bool bDone, std::string& sMessage, std::string& sAnalyticQueueInAddress, std::string& sAnalyticQueueOutAddress);
+	static void extractKillAllAnalyticProcessesReplyData(const std::string& sReply, bool& sDone, std::string& sMessage);
+	static void extractStopAnalyticProcessesReplyData(const std::string& sReply, bool& bDone, std::string& sMessage);
+	void extractErrorReplyData(const std::string& sReply, std::string& sOperation, bool bDone, std::string& sMessage);
 
-	static std::string getAnalyticStopRequest(unsigned int iAnalyticInstanceId);
-	static std::string getAnalyticStopReply(bool bDone);
+	//Request XML Messages
+	static std::string getAnalyticStartRequest(const unsigned int iAnalyticInstanceId, const std::string& sAnalyticPluginDirLocation, const std::string& sAnalyticPluginFilename);
+	static std::string getAnalyticStopRequest(const unsigned int iAnalyticInstanceId);
+	static std::string getKillAllAnalyticProcessesRequest();
+
+	//Reply XML Messages
+	static std::string getAnalyticStartReply(const bool bDone, const std::string& sMessage, const std::string& sAnalyticQueueInAddress,const std::string& sAnalyticQueueOutAddress);
+	static std::string getAnalyticStopReply(const bool bDone, const std::string& sMessage);
+	static std::string getKillAllAnalyticProcessesReply(const bool bDone, const std::string& sMessage);
+	static std::string getErrorReply(const std::string& sOperation, const bool bDone, const std::string& sErrorMessage);
 
 	static std::string getPidMessage(pid_t pid);
-
-	static std::string getKillAllAnalyticProcessesRequest();
-	static std::string getKillAllAnalyticProcessesReply(bool bDone);
-
-	static void extractAnalyticStartRequestData(const std::string& sAnalyticStartRequest, unsigned int& iAnalyticInstanceId, std::string& sAnalyticDirLocation, std::string& sAnalyticFilename);
-	static void extractAnalyticStartReplyData(const std::string& sAnalyticStartReply, std::string& sAnalyticQueueInAddress, std::string& sAnalyticQueueOutAddress);
-	static void parseKillAllAnalyticProcessesReply(const std::string& sReply, bool& sDone);
-	static void parseStopAnalyticProcessesReply(const std::string& sReply, bool& sDone);
 	static pid_t getPid(const std::string& sPidMessage);
 };
 
