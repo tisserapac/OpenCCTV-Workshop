c9efdecf3923992df2e0817de944236943bfb70e
Anjana Tissera <anjanat@gmail.com>
2015-07-01 01:10:33 +0700

    The socket created in the connectToMq() function modified to have a receive timeout of 100 ms.

--- OpenCCTVServer/src/opencctv/mq/MqUtil.cpp
+++ OpenCCTVServer/src/opencctv/mq/MqUtil.cpp
@@ -34,12 +34,11 @@ zmq::socket_t* MqUtil::connectToMq(const std::string& serverName, const std::str
 	zmq::socket_t* pSocket = NULL;
 	try {
 		pSocket = new zmq::socket_t(*pContext, zmqClientSocketType);
-		/*int timeout = 100;
-		pSocket->setsockopt(ZMQ_SNDTIMEO, &timeout, sizeof(timeout));*/
+		int timeout = 100;
+		pSocket->setsockopt(ZMQ_RCVTIMEO, &timeout, sizeof(timeout));
 
 		if (pSocket) {
-			pSocket->connect(
-					getZmqServerConnectUrl(serverName, serverPortStr).data());
+			pSocket->connect(getZmqServerConnectUrl(serverName, serverPortStr).data());
 		}
 	} catch (error_t &e) {
 		std::string sErrMsg = "Failed to connect to MQ at: ";
